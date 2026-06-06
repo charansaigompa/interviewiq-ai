1cefaf387950dc625ea8b5045392b010c5f6c55f
charansaigompa <gompacharansai10@gmail.com>
2026-06-06 15:22:32 +0530

    setup backend server

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from "express"
+import dotenv from "dotenv"
+import { connect } from "mongoose"
+import connectDb from "./config/db.js"
+import authRouter from "./routes/auth.routes.js"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import userRouter from "./routes/user.route.js"
+dotenv.config()
+const app=express()
+
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+    
+}))
+app.use(express.json())
+app.use(cookieParser())
+
+
+const PORT=process.env.PORT || 6000
+app.use("/api/auth",authRouter)
+app.use("/api/user",userRouter)
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+    connectDb()
+})
