fb4bc61562834ef76ada2a7981b6e8bc6698694e
charansaigompa <gompacharansai10@gmail.com>
2026-06-14 19:26:53 +0530

    added get-interviews and report route

--- server/routes/interview.route.js
+++ server/routes/interview.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import isAuth from "../middlewares/isAuth.js"
-import { analyzeResume, finishInterview, generateQuestion, submitAnswer } from "../controllers/interview.controller.js"
+import { analyzeResume, finishInterview, generateQuestion, getInterviewReport, getMyInterviews, submitAnswer } from "../controllers/interview.controller.js"
 import { upload } from "../middlewares/multer.js"
 
 const interviewRouter=express.Router()
@@ -8,5 +8,9 @@ interviewRouter.post("/resume",isAuth,upload.single("resume"),analyzeResume)
 interviewRouter.post("/generate-questions",isAuth,generateQuestion)
 interviewRouter.post("/submit-answer",isAuth,submitAnswer)
 interviewRouter.post("/finish",isAuth,finishInterview)
+interviewRouter.get("/get-interviews",isAuth,getMyInterviews)
+interviewRouter.get("/report/:id",isAuth,getInterviewReport)
+
+
 
 export default interviewRouter
