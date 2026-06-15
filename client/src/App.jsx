3d0f3bb26dea4354daef405f266375cab92b88c7
Charan Sai Gompa <gompacharansai10@gmail.com>
2026-06-15 19:43:51 +0530

    Changed serverUrl to production endpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const serverUrl="http://localhost:8000"
+export const serverUrl="https://interviewiq-ai-fo4w.onrender.com"
 const App = () => {
   const dispatch=useDispatch()
 
